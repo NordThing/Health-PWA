b132470dc099192428aebdef2d3caa5c6def643f
henke <henke@garage.garage>
2020-11-27 13:42:46 +0100

    updated file

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,9 @@
-
+//LOADER FOR SERVICE WORKER FOR PWA
+window.onload = () => {
+    'use strict';
+  
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+               .register('./sw.js');
+    }
+  }
