3abe8f3e7b54e31185d7b0ee3f02399249ade780
henke <henke@garage.garage>
2020-12-02 21:41:41 +0100

    nothing important

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello World</p>'; ?>
+ <?php echo '<p>header.php</p>'; ?>
+ <?php echo '<p>main-content.php</p>'; ?>
+ <?php echo '<p>footer.php</p>'; ?> 
  </body>
 </html>
