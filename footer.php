d9ada8875665124ef3777bb8bf97c65fa79111f4
henke <henke@garage.garage>
2020-12-02 21:52:18 +0100

    Splitting pages

---
+++ footer.php
@@ -0,0 +1,32 @@
+    <!-- FOOTER -->
+    <footer>
+        <div class="footer">    
+            <div class="row">
+                <div class="column">
+                    <div class="footer-overview">
+                        <i class="material-icons">dvr</i>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="footer-calendar">
+                        <i class="material-icons">calendar_today</i>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="footer-activity">
+                        <i class="material-icons">timer</i>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="footer-weight">
+                        <i class="material-icons">addchart</i>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    </footer>
+</div>
+<script src="js/custom.js"></script>
+<script src="js/stopwatch.js"></script>
+</body>
+</html>
