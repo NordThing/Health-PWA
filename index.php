ae6c9073970634baea078fc26be9448296a77718
henke <henke@garage.garage>
2020-12-02 22:10:14 +0100

    Fixed index.php

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?>
- <?php echo '<p>header.php</p>'; ?>
- <?php echo '<p>main-content.php</p>'; ?>
- <?php echo '<p>footer.php</p>'; ?> 
- </body>
-</html>
+<?php include 'header.html';?>
+<?php include 'main-content.html';?>
+<?php include 'footer.html';?>
