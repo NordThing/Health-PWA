94775160eac2c8e409375a20e2f625708b390c36
henke <henke@garage.garage>
2020-12-05 13:22:17 +0100

    Preparing to change the contents with PHP script

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php include 'header.html';?>
+<?php include 'header.html' ;?>
 
-<?php include 'main-content.html';?>
+<?php 
+$t = date("H");
 
+if ($t < "20") {
+    echo "Have a good day!";
+  } else {
+    echo "Have a good night!";
+  }
+  ?>
+  
 <?php include 'footer.html';?>
