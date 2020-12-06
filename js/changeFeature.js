35e5f48b503a28aa7b8563e83eed0fe15b40aa1a
henke <henke@garage.garage>
2020-12-06 20:16:13 +0100

    Changed href to buttons - added styling for darkmode

--- js/changeFeature.js
+++ js/changeFeature.js
@@ -3,9 +3,9 @@ function changeFeature() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("demo").innerHTML = this.responseText;
+        document.getElementById("main").innerHTML = this.responseText;
       }
     };
-    xhttp.open("GET", "test.html", true);
+    xhttp.open("GET", "main-overview.html", true);
     xhttp.send();
   }
