01a5ecc09b8a913ebb57621896bd66ac66c48035
shane.kakau <shane.kakau@cgm.com>
2020-12-15 16:45:30 +0100

    fixed darkmode toggle

--- js/darkmode.js
+++ js/darkmode.js
@@ -2,34 +2,23 @@ var sw = document.querySelector('#checkbox');
 
 var current_scheme = localStorage.getItem('theme');
 
-if(current_scheme)
-{
-  document.documentElement.setAttribute('data-theme', current_scheme);
-  if(current_scheme === 'dark')
-  {
-   sw.checked = true;
-  }
-  else{
-   sw.checked = false;
-  }
-}
-else{
- // user didn't saved it or he hates dark mode
+if(current_scheme) {
+    document.documentElement.setAttribute('data-theme', current_scheme);
+    if(current_scheme === 'dark') {
+        sw.checked = true;
+    } else{
+        sw.checked = false;
+    }
 }
 
-// alert(current_scheme)
-
-function changeTheme()
-{
-if(sw.checked){
- document.documentElement.setAttribute('data-theme', 'dark');
- localStorage.setItem('theme', 'dark');
+function changeTheme() {
+    if (!sw.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        sw.checked = true;
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light'); // or just empty " "
+        localStorage.setItem('theme', 'light'); // or just empty " "
+        sw.checked = false;
+    } 
 }
-else{
- document.documentElement.setAttribute('data-theme', 'light'); // or just empty " "
- localStorage.setItem('theme', 'light'); // or just empty " "
-} 
-  // alert(document.documentElement.getAttribute('data-color-scheme'))
-}
-
- sw.addEventListener('change', function(){ changeTheme() });
