33308402dd9efcb1b75004ad2c7e19410dd14ed6
henke <henke@garage.garage>
2021-01-02 09:44:02 +0100

    Added comments in js

--- js/coach.js
+++ js/coach.js
@@ -24,3 +24,23 @@ setTimeout(stopTimer,timeGoal);
 function stopTimer() {
     clearInterval(myVar);
 }
+
+/* ------------------------------- */
+/* THIS IS THE TEXT TO SPEECH PART */
+/* ------------------------------- */
+var synth = window.speechSynthesis;
+
+var voices  = synth.getVoices();
+
+var t = "You have runned for 5 minutes";
+
+var u = new SpeechSynthesisUtterance(t);
+
+function test(){
+document.getElementById('doSpeak').onclick = function() {
+    synth.speak(u);
+    setInterval(function () {
+        console.log("Testing");
+    }, 100)
+}
+}
