fe7c68dbf10ce724bbf831b0237d3cf43c2da516
henke <henke@garage.garage>
2021-01-05 22:57:27 +0100

    Adding some more js for changing title in headline

--- js/changeTitle.js
+++ js/changeTitle.js
@@ -1,7 +1,25 @@
 var myClasses = document.getElementsByClassName("header-app-icon");
 
-function changeTitle() {
+function changeTitle0() {
+    for (var i = 0; i < myClasses.length; i++) {
+        myClasses[i].innerHTML = "";
+        }
+}
+
+function changeTitle1() {
     for (var i = 0; i < myClasses.length; i++) {
         myClasses[i].innerHTML = "<h6>Latest Activity</h6>";
+        }
+}
+
+function changeTitle2() {
+    for (var i = 0; i < myClasses.length; i++) {
+        myClasses[i].innerHTML = "<h6>History</h6>";
+        }
+}
+
+function changeTitle3() {
+    for (var i = 0; i < myClasses.length; i++) {
+        myClasses[i].innerHTML = "<h6>Weight</h6>";
         }
 }
