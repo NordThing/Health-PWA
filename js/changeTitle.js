8c007b35a69bda3ad660c10e5e7380528d6a807c
henke <henke@garage.garage>
2021-01-05 22:48:20 +0100

    Removed headlines for feature 2

--- js/changeTitle.js
+++ js/changeTitle.js
@@ -2,6 +2,6 @@ var myClasses = document.getElementsByClassName("header-app-icon");
 
 function changeTitle() {
     for (var i = 0; i < myClasses.length; i++) {
-        myClasses[i].innerHTML = "Latest Activity";
+        myClasses[i].innerHTML = "<h6>Latest Activity</h6>";
         }
 }
